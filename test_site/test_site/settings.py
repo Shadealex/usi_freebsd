bf34e2a5f1e3d9901b564b79c3424e0a29addbd3
Shadealex <5hnm@ukr.net>
2015-09-15 11:47:10 +0300

    fix static in html

--- test_site/test_site/settings.py
+++ test_site/test_site/settings.py
@@ -110,3 +110,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = 'static'
+MEDIA_ROOT = 'media'
+
+STATICFILES_DIRS = (
+    # os.path.join(BASE_DIR, 'static'),
+    'staic',
+    'media',
+)
